53200de2057394641776c237f8d30621af16d5ca
Him1998 <57448709+Him1998@users.noreply.github.com>
2023-03-05 12:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.set('strictQuery', true)
+
 app.use("/contact", (req, res, next) => {
   const token = req.headers.authorization;
 
